b4c451b73389d599e79685aa6f4192a2d5df0f46
Frazonar <snip.coloured@gmail.com>
2022-01-02 20:02:43 +0200

    feat: create users controller

---
+++ src/controllers/users.js
@@ -0,0 +1,55 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+const NotFoundError = require('../errors/not-found-err');
+const { secretKey } = require('../utils/constants');
+
+const getCurrentUser = (req, res, next) => {
+  User.findById(req.user._id)
+    .orFail(new NotFoundError())
+    .then((user) => {
+      res.status(200).send({ data: user });
+    })
+    .catch(next);
+};
+
+const createUser = (req, res, next) => {
+  const {
+    email,
+    password,
+    name,
+  } = req.body;
+
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      email,
+      password: hash,
+      name,
+    }))
+    .then((user) => {
+      res.status(201).send({ data: user });
+    })
+    .catch(next);
+};
+
+const login = (req, res, next) => {
+  const {
+    email,
+    password,
+  } = req.body;
+
+  User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, secretKey, { expiresIn: '7d' });
+
+      res.status(200).send({ token });
+    })
+    .catch(next);
+};
+
+module.exports = {
+  getCurrentUser,
+  createUser,
+  login,
+};
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const urlRegex = /^https?:\/{2}(www\.)?[a-z\0-9]{2,256}\.[a-z]{2,6}(\/[a-z0-9._~:/?%#[\]@!$&'()*+,;=]*)?/i;
 
 const responseStrings = {
@@ -17,8 +19,12 @@ const errorStrings = {
   blacklistedEmailProvider: 'Blacklisted e-mail provider',
 };
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+const secretKey = NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key';
+
 module.exports = {
   urlRegex,
   responseStrings,
   errorStrings,
+  secretKey,
 };
